7b5370c018bd7f08e1d4e3a1a5349255801761ef
flaketill <armandoibarra1@gmail.com>
2023-04-24 20:30:22 -0600

    ADD: Variables.

---
+++ variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+name = "Armando"
+last_name = "Ibarra"
+age = 25
+list_shopping = ("Milk", "Eggs", "Vegetables")
+
+print(f"{name} {last_name} {age}")
+
+
+print(list_shopping)
