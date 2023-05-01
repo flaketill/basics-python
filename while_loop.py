9d69893477e1806c24f5a086210e7dc929d9a80f
flaketill <armandoibarra1@gmail.com>
2023-04-30 20:55:44 -0600

    UPD: control flow while and break

--- while_loop.py
+++ while_loop.py
@@ -27,9 +27,38 @@ def get_team_info_rows(*members):
     return '\n '.join(team_info)
 
 
+def search_member(name, **members):
+    team = []
+    index = 0
+    items = members.items()
+    itr = iter(items)
+    
+    
+    while index < len(members.items()):
+        item = next(itr)
+        key, value = item[0], item[1]
+        
+        if key == name:            
+            #return f"{key}: {value}"
+            return value
+            break
+
+        index += 1
+
+    return ', '.join(team)
+
+
 if __name__ == '__main__':
 
 
     print(f"{get_team_members('Armando', 'Danna', 'Keyra', 'Dante')}")
 
+
     print(f"\n The team info: \n {get_team_info_rows('Armando', 'Danna', 'Keyra', 'Dante')}")
+    
+    
+    print(f"\n Searching Armando index: \n {search_member(name='Armando', Armando=0, Danna=1, Keyra=2, Dante=3)}" )
+
+    
+    print(f"\n Searching Danna index: \n {search_member(name='Danna', Armando=0, Danna=1, Keyra=2, Dante=3)}" )
+
