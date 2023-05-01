5f0f733841ee4393ab56c29af36a80f6ef18df79
flaketill <armandoibarra1@gmail.com>
2023-04-30 20:19:31 -0600

    UPD: control flow while and for range

--- while_loop.py
+++ while_loop.py
@@ -13,8 +13,23 @@ def get_team_members(*members):
     return ', '.join(team)
 
 
+def get_team_info_rows(*members):
+    team_info = []
+    members = members
+    rows = [ index for index in range(1, len(members)+1)]
+    index = 0
+
+    
+    while index < len(members):
+        team_info.append(f"{rows[index]} - {members[index]}")
+        index += 1
+        
+    return '\n '.join(team_info)
+
+
 if __name__ == '__main__':
 
 
     print(f"{get_team_members('Armando', 'Danna', 'Keyra', 'Dante')}")
-    
+
+    print(f"\n The team info: \n {get_team_info_rows('Armando', 'Danna', 'Keyra', 'Dante')}")
