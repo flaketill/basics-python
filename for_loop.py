a23086e9d45d1e29bda6df683f49171baa987d4e
flaketill <armandoibarra1@gmail.com>
2023-04-30 20:00:17 -0600

    UPD: control flow for with zip

--- for_loop.py
+++ for_loop.py
@@ -21,6 +21,18 @@ def get_team_members_list(*members):
     return '\n '.join(team)
 
 
+def get_team_info_rows(*members):
+    team_info = []
+    members = members
+    rows = [ index for index in range(1, len(members)+1)]
+
+    
+    for member, row  in zip(members,rows):
+        team_info.append(f"{row} - {member}")
+        
+    return '\n '.join(team_info)
+
+
 if __name__ == '__main__':
 
 
@@ -29,4 +41,5 @@ if __name__ == '__main__':
 
     print(f"\n Exists the next rows: \n {get_team_members_list('Armando', 'Danna', 'Keyra', 'Dante')}")
 
-    
+
+    print(f"\n The team info: \n {get_team_info_rows('Armando', 'Danna', 'Keyra', 'Dante')}")
