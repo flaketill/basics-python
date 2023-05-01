696006d736e8f8f1398cf2b87246a74e232ea269
flaketill <armandoibarra1@gmail.com>
2023-04-30 19:59:33 -0600

    UPD: control flow for with range

--- for_loop.py
+++ for_loop.py
@@ -11,7 +11,22 @@ def get_team_members(*members):
 	return ', '.join(team)
 
 
+def get_team_members_list(*members):
+    team = []
+    
+    
+    for index in range(1, len(members)+1):
+        team.append(f"{index}")
+        
+    return '\n '.join(team)
+
+
 if __name__ == '__main__':
 
 
     print(f"{get_team_members('Armando', 'Danna', 'Keyra', 'Dante')}")
+
+
+    print(f"\n Exists the next rows: \n {get_team_members_list('Armando', 'Danna', 'Keyra', 'Dante')}")
+
+    
